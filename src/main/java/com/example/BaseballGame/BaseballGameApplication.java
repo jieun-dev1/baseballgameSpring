ec15910a0e74e73e0d90708d6419687f4ef44870
jieun-dev1 <jkdev1324@gmail.com>
2022-08-25 00:04:24 +0900

    Feat: 기타 Infrastructure 코드
    Answer 생성
    -Fixed 구현체를 통해서, 채점이 잘 되는지 테스트.
    -숫자 야구 게임의 룰상 각 번호는 겹치지 않아야 하므로, 이에 맞게 처리.

--- src/main/java/com/example/BaseballGame/BaseballGameApplication.java
+++ src/main/java/com/example/BaseballGame/BaseballGameApplication.java
@@ -1,8 +1,21 @@
 package com.example.BaseballGame;
 
+import com.example.BaseballGame.infrastructure.config.ServiceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+/*
+SpringBootApplication 안에 @ComponentScan이 붙어 있다.
+SpringBootApplication 을 쓰지 않을 경우 ApplicationContext 직접 만들어 쓰는 법도 있다.
+
+@ComponentScan 은 @Component 가 붙은 모든 클래스를 스프링 빈으로 등록한다.
+이때 스프링 빈의 기본 이름은 클래스명을 사용하되 맨 앞글자만 소문자를 사용한다.
+
+스프링 컨테이너는 @Configuration 이 붙은 Config 를 설정(구성) 정보로 사용한다.
+@Bean 이라 적힌 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록한다.
+이렇게 스프링 컨테이너에 등록된 객체를 스프링 빈이라 한다
+ */
 @SpringBootApplication
 public class BaseballGameApplication {
 
--- src/main/java/com/example/BaseballGame/infrastructure/IncrementalGameIdGenerator.java
+++ src/main/java/com/example/BaseballGame/infrastructure/IncrementalGameIdGenerator.java
@@ -1,14 +1,14 @@
-package com.example.BaseballGame.application;
+package com.example.BaseballGame.infrastructure;
 
 
 import com.example.BaseballGame.domain.GameIdGenerator;
 import java.util.concurrent.atomic.AtomicLong;
 
 /*
-특정 범위(ex.1-9)까지의 숫자로 랜덤숫자를 만듭니다.
+100부터 순차적으로 Id가 증가함. Id 생성 전략이 바뀔 가능성 대비하여 구현체 생성.
 volatile은 읽기 쓰기에 대한 동기화를 보장하고 연산에 대해서는 동기화를 보장하지 않으므로 AtomicLong을 사용.
  */
-public class GameIdGeneratorImpl implements GameIdGenerator {
+public class IncrementalGameIdGenerator implements GameIdGenerator {
     private AtomicLong id = new AtomicLong(100L);
 
     @Override
